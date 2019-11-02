705d9cbb7d32f4c741e4464d83c9038c2c4ad14a
mike881205 <mike881205@gmail.com>
2019-11-01 20:12:47 -0400

    Created selection objects for each planet, removed 'POI' dropdown menu,  created click functions to filter planets by Terrain, added show all button to show filtered hidden planets

--- assets/javascript/first-selection.js
+++ assets/javascript/first-selection.js
@@ -1,47 +1,100 @@
-let desert  = $("#desert")
-let frozen  = $("#frozen")
-let tropical  = $("#tropical")
-let forrest = $("#forrest")
+let desert  = {
+    dropDown: $("#desert"),
+    menu: {
+        icon: $("#desertPlanets"),
+        hidden: false
+    }
+}
 
-let terrainOptions = [desert, frozen, tropical, forrest]
+let frozen  = {
+    dropDown: $("#frozen"),
+    menu: {
+        icon: $("#frozenPlanets"),
+        hidden: false
+    }
+}
 
-let selection = $(".dropdown-item")
+let tropical  = {
+    dropDown: $("#tropical"),
+    menu: {
+        icon: $("#tropicalPlanets"),
+        hidden: false
+    }
+}
 
-selection.on("click", function(){
-    for (let i = 0; i < terrainOptions.length; i++) {
-        if (selection = desert) {
-            console.log("hello")
-            $("#frozenPlanets").hide();
-            $("#tropicalPlanets").hide();
-            $("#forrestPlanets").hide();
+let forrest  = {
+    dropDown: $("#forrest"),
+    menu: {
+        icon: $("#forrestPlanets"),
+        hidden: false
+    }
+}
+
+let selectArray = [desert, frozen, tropical, forrest]
+
+desert.dropDown.on("click", function(){
+    if (desert.menu.hidden = true) {
+        desert.menu.icon.show();
+        desert.menu.hidden = false;
+    }
+    console.log("desert")
+    frozen.menu.icon.hide();
+    frozen.menu.hidden = true
+    tropical.menu.icon.hide();
+    tropical.menu.hidden = true
+    forrest.menu.icon.hide();
+    forrest.menu.hidden = true
+})
+
+frozen.dropDown.on("click", function(){
+    console.log("frozen")
+    if (frozen.menu.hidden = true) {
+        frozen.menu.icon.show();
+        frozen.menu.hidden = false;
+    }
+    desert.menu.icon.hide();
+    desert.menu.hidden = true
+    tropical.menu.icon.hide();
+    tropical.menu.hidden = true
+    forrest.menu.icon.hide();
+    forrest.menu.hidden = true
+})
+
+tropical.dropDown.on("click", function(){
+    console.log("tropical")
+    if (tropical.menu.hidden = true) {
+        tropical.menu.icon.show();
+        tropical.menu.hidden = false;
+    }
+    desert.menu.icon.hide();
+    desert.menu.hidden = true
+    frozen.menu.icon.hide();
+    frozen.menu.hidden = true
+    forrest.menu.icon.hide();
+    forrest.menu.hidden = true
+})
+
+forrest.dropDown.on("click", function(){
+    console.log("forrest")
+    if (forrest.menu.hidden = true) {
+        forrest.menu.icon.show();
+        forrest.menu.hidden = false;
+    }
+    desert.menu.icon.hide();
+    desert.menu.hidden = true
+    frozen.menu.icon.hide();
+    frozen.menu.hidden = true
+    tropical.menu.icon.hide();
+    tropical.menu.hidden = true
+})
+
+$("#showAllBtn").on("click", function(){
+    for (let i = 0 ; i < selectArray.length; i++) {
+        if (selectArray[i].menu.hidden = true) {
+            selectArray[i].menu.hidden = false;
+            selectArray[i].menu.icon.show();
+        } else {
+            return
         }
     }
 })
-
-// desert.on("click", function(){
-//     console.log("hello")
-//     $("#frozenPlanets").hide();
-//     $("#tropicalPlanets").hide();
-//     $("#forrestPlanets").hide();
-// })
-
-// frozen.on("click", function(){
-//     console.log("hello")
-//     $("#desertPlanets").hide();
-//     $("#tropicalPlanets").hide();
-//     $("#forrestPlanets").hide();
-// })
-
-// tropical.on("click", function(){
-//     console.log("hello")
-//     $("#desertPlanets").hide();
-//     $("#frozenPlanets").hide();
-//     $("#forrestPlanets").hide();
-// })
-
-// forrest.on("click", function(){
-//     console.log("hello")
-//     $("#desertPlanets").hide();
-//     $("#frozenPlanets").hide();
-//     $("#tropicalPlanets").hide();
-// })
